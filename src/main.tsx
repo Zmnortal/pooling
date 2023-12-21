4e93bf78cbf392d825d6f509234e3c734edb5b50
Zmnortal <276678881@qq.com>
2023-12-21 12:59:48 +0800

    dist only

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './styles/tailwind.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-)
